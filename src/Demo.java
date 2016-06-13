c91bf46db2f26ead2abd35ee4ef50f700d1e3dd4
liao930211 <liao930211@163.com>
2016-06-13 14:21:05 +0800

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -4,6 +4,6 @@
 public class Demo {
 
     public static void main(String[] args){
-        System.out.println("这是第三个Gitup的Demo");
+        System.out.println("http://www.runningcheese.com/hosts");
     }
 }
